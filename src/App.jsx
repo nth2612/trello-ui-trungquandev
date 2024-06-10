f415400c5742c060fd42dd9904be1c1029bca0b9
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-06-10 13:53:31 +0700

    hoc ve dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import { Typography } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle/>
+
+      <hr />
+
       <div>nth2612</div>
 
       <Typography variant='body2' color="text.secondary"> Test Typography</Typography>
