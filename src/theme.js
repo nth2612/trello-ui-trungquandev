40d1ad7ed6f92c1423ebc372d0a2c365e5c12890
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-06-11 14:09:06 +0700

    Xay dung layout

--- src/theme.js
+++ src/theme.js
@@ -3,18 +3,24 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange
       }
+      // spacing: (factor) => `${0.25 * factor}rem`
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
       }
+      // spacing: (factor) => `${0.25 * factor}rem`
     }
   }
 })
